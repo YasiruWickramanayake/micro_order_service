acf23f073b96e5261613a136dfcb536c019e8eb3
yasiruw <w.yasiru@directfn.com>
2024-08-31 14:31:23 +0300

    change done

--- orderServiceContainer/src/main/java/com/micro/orderservicecontainer/OrderServiceContainerApplication.java
+++ orderServiceContainer/src/main/java/com/micro/orderservicecontainer/OrderServiceContainerApplication.java
@@ -1,19 +1,17 @@
-package com.micro.orderService.builder;
+package com.micro.orderservicecontainer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.stereotype.Repository;
 
-@SpringBootApplication(scanBasePackages = {"com.micro.orderService.*"})
+@SpringBootApplication(scanBasePackages = {"com.micro.orderService.*", "com.micro.orderservicecontainer.*"})
 @EntityScan(basePackages = {"com.micro.orderService.*"})
 @EnableJpaRepositories(basePackages = {"com.micro.orderService.*"})
-public class OrderServiceApplication {
+public class OrderServiceContainerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OrderServiceApplication.class, args);
+        SpringApplication.run(OrderServiceContainerApplication.class, args);
     }
 
 }
---
+++ orderServiceContainer/src/test/java/com/micro/orderservicecontainer/OrderServiceContainerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.micro.orderservicecontainer;
+
+
+
+
+
+class OrderServiceContainerApplicationTests {
+
+
+
+}
